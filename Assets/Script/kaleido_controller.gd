61ed0b80e31d9c55cb39026d854ea3c906a7bbcb
Gale <cesarmavarest@gmail.com>
2025-09-16 23:42:44 -0400

    Maquetado del primer nivel, ajustes de gravedad y camara, configuraciones globales, borrar configuracion individual de camara y sprites de monedas para adaptarse a cada nivel

--- Assets/Script/kaleido_controller.gd
+++ Assets/Script/kaleido_controller.gd
@@ -1,31 +1,46 @@
 extends CharacterBody2D
 class_name KaleidoController
 
-@export var speed = 10.0
-@export var jump_power = 10.0
+@export var speed = 15.0
+@export var jump_power = 30.0
+@export var double_jump_power = 30.0 # Nueva variable para el poder del doble salto
+@export var coyote_time_duration = 0.25 # Duración del coyote time en segundos
 
 var speed_mult = 30.0
 var jump_mult = -30.0
 var direction = 0
-
-#const SPEED = 300.0
-#const JUMP_VELOCITY = -400.0
+var double_jump_available = true # Variable para controlar si el doble salto está disponible
+var coyote_time_counter = 0.0 # Contador para el coyote time
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	# Add the gravity.
+	# Añade la gravedad.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		coyote_time_counter -= delta # Decrementa el contador de coyote time
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_power * jump_mult
+	# Reinicia el doble salto y el coyote time cuando el personaje está en el suelo.
+	if is_on_floor():
+		double_jump_available = true
+		coyote_time_counter = coyote_time_duration
+
+	# Maneja el salto.
+	if Input.is_action_just_pressed("jump"):
+		# Salto normal: si el personaje está en el suelo o dentro del coyote time.
+		if is_on_floor() or coyote_time_counter > 0:
+			velocity.y = jump_power * jump_mult
+			# Reinicia el contador para evitar saltos repetidos después de un solo salto en el suelo
+			coyote_time_counter = 0
+
+		# Doble salto: si el personaje no está en el suelo y el doble salto está disponible.
+		elif not is_on_floor() and double_jump_available:
+			velocity.y = double_jump_power * jump_mult
+			double_jump_available = false # Deshabilita el doble salto
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * speed * speed_mult
